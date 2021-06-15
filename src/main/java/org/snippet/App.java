8a8b01a552825a8fedb705e22cf8a17c3a949596
kofyou <ofyou6@outlook.com>
2021-06-15 21:27:48 +0800

    输出报告

--- src/main/java/org/snippet/App.java
+++ src/main/java/org/snippet/App.java
@@ -54,12 +54,8 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -68,8 +64,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -112,7 +107,7 @@ public class App {
 
 	private static void readJson() {
         Gson gson = new Gson();
-        try (Reader reader = new FileReader(OUTPUT_JSONS + today.minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE))) {
+        try (Reader reader = new FileReader(OUTPUT_JSONS + today.minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE), StandardCharsets.UTF_8)) {
         	Type listType = new TypeToken<ArrayList<ReportRecord>>(){}.getType();
             report = gson.fromJson(reader, listType);
         } catch (IOException e) {
@@ -122,7 +117,7 @@ public class App {
 	
 	private static void writeJson() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try (FileWriter writer = new FileWriter(OUTPUT_JSONS + today.format(DateTimeFormatter.ISO_LOCAL_DATE))) {
+        try (FileWriter writer = new FileWriter(OUTPUT_JSONS + today.format(DateTimeFormatter.ISO_LOCAL_DATE), StandardCharsets.UTF_8)) {
             gson.toJson(report, writer);
         } catch (IOException e) {
             e.printStackTrace();
@@ -157,14 +152,49 @@ public class App {
 			LocalDate yesterday = today.minusDays(1);
 			if(commentDate.equals(yesterday)) {
 				dailyNew++;
-				System.out.println(comment);
+//				try (FileWriter writer = new FileWriter(OUTPUT_COMMENTS + today.format(DateTimeFormatter.ISO_LOCAL_DATE), StandardCharsets.UTF_8)) {
+//		    	    writer.write(comment + "\n");
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
 			}
 		}
 		return dailyNew;
 	}
     
     private static void writeReport() {
-		
+    	try (FileWriter writer = new FileWriter(OUTPUT_REPORTS + today.format(DateTimeFormatter.ISO_LOCAL_DATE), StandardCharsets.UTF_8)) {
+    	    writer.write(String.valueOf(Character.toChars(0x2666)) + "点评和问题收集打卡\n");
+    	    
+    	    writer.write("\n" + String.valueOf(Character.toChars(0x1F680)) + today.format(DateTimeFormatter.ISO_LOCAL_DATE) + "当日点评排行榜\n");
+    	    writer.write("---\n");
+    	    
+    	    Collections.sort(report, (ReportRecord r1, ReportRecord r2) -> r2.dailyNew - r1.dailyNew);
+    	    for(ReportRecord record : report) {
+    	    	if(record.dailyNew != 0) {
+    	    		writer.write(record.name + "：" + record.dailyNew + "\n");
+    	    	}
+    	    }
+    	    
+    	    writer.write("\n" + String.valueOf(Character.toChars(0x1F31F)) + "点评汇总排行榜\n");
+    	    writer.write("---\n");
+    	    Collections.sort(report, (ReportRecord r1, ReportRecord r2) -> r2.dailyNew + r2.total - r1.dailyNew - r1.total);
+    	    for(ReportRecord record : report) {
+    	    	if(record.dailyNew != 0) {
+    	    		writer.write(record.name + "：" + record.link + " " + "点评数：" + record.total + "+" +record.dailyNew + String.valueOf(Character.toChars(0x1F195)) + "\n\n");
+    	    	} else {
+    	    		writer.write(record.name + "：" + record.link + " " + "点评数：" + record.total + "\n\n");
+    	    	}
+    	    }
+    	    
+    	    writer.write(String.valueOf(Character.toChars(0x1F914)) + "团队问题列表\n");
+    	    writer.write("---\n");
+    	    writer.write("https://github.com/FZUSESPR21W/Class_Resources/blob/main/%E5%90%84%E5%9B%A2%E9%98%9F%E9%97%AE%E9%A2%98%E6%94%B6%E9%9B%86.md");
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
     
 	public static List<String> comment(String user, int pageIndex, int pageSize) {
